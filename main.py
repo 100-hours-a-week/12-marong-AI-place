9a5e84e2647575676d0420d6976c7a0496a6ebc0
KIMSS <kcg199510@gmail.com>
2025-05-01 14:13:25 +0900

    refactor: add module for convinience

--- main.py
+++ main.py
@@ -2,7 +2,6 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import List
-import torch
 from sentence_transformers import SentenceTransformer
 from RecommendPlace import RecommendPlace
 from mbti_projector import MBTIProjector
@@ -10,6 +9,9 @@ from chromadb import PersistentClient
 from chromadb.config import Settings
 from datetime import datetime, timedelta
 from get_week_index import GetWeekIndex
+from average_latlng import AverageLatLng
+import torch
+import math
 
 # 기준일: 2025년 5월 12일 (월요일)
 base_date = datetime(2025, 5, 12)
@@ -52,42 +54,22 @@ class PairInput(BaseModel):
     me: RecommendInput
     manitto: RecommendInput
 
-import math
-
-def average_latlng(lat1, lon1, lat2, lon2):
-    # 위도/경도를 라디안으로 변환
-    lat1, lon1, lat2, lon2 = map(math.radians, [lat1, lon1, lat2, lon2])
-
-    # 위도/경도를 3D 좌표로 변환
-    x1, y1, z1 = math.cos(lat1)*math.cos(lon1), math.cos(lat1)*math.sin(lon1), math.sin(lat1)
-    x2, y2, z2 = math.cos(lat2)*math.cos(lon2), math.cos(lat2)*math.sin(lon2), math.sin(lat2)
-
-    # 평균 벡터
-    x, y, z = (x1 + x2) / 2, (y1 + y2) / 2, (z1 + z2) / 2
-
-    # 다시 위도/경도로 변환
-    lon = math.atan2(y, x)
-    hyp = math.sqrt(x * x + y * y)
-    lat = math.atan2(z, hyp)
-
-    # 라디안 → 도(degree)
-    return math.degrees(lat), math.degrees(lon)
-
 # 추천 API 엔드포인트
 @app.post("/recommend/place")
 def recommend_places(input: PairInput):
-    # ✅ 평균 벡터 계산
+    # 평균 벡터 계산
     avg_vector = [
         (input.me.eiScore + input.manitto.eiScore) / 2,
         (input.me.snScore + input.manitto.snScore) / 2,
         (input.me.tfScore + input.manitto.tfScore) / 2,
         (input.me.jpScore + input.manitto.jpScore) / 2,
     ]
+    # 평균 위치
+    average_loc = AverageLatLng(input.me.latitude, input.me.longitude, input.manitto.latitude, input.manitto.longitude)
+    average_loc.loc_to_vec()
+    avg_lat, avg_lng = average_loc.get()
 
-    # ✅ 평균 위치
-    avg_lat, avg_lng = average_latlng(input.me.latitude, input.me.longitude,  input.manitto.latitude, input.manitto.longitude)
-
-    # ✅ 취향 합치기
+    # 취향 합치기
     like_foods = list(set(input.me.likedFoods + input.manitto.likedFoods))
     dislike_foods = list(set(input.me.dislikedFoods + input.manitto.dislikedFoods))
 
