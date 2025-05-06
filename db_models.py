7a7cfa40018e22818041d063bfa60b78039af5ba
KIMSS <kcg199510@gmail.com>
2025-05-06 21:39:31 +0900

    feat: 마니또 자동 매칭 스크립트 추가,  백엔드 DB에서 데이터 로딩,  장소 추천 결과 적재 로직 추가

---
+++ db_models.py
@@ -0,0 +1,55 @@
+from sqlalchemy import Column, BigInteger, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+
+Base = declarative_base()
+
+class Users(Base):
+    __tablename__ = "Users"
+    id = Column(BigInteger, primary_key=True, autoincrement=True)
+    email = Column(String(100), nullable=False)
+    provider_id = Column(String(100), nullable=False)
+    nickname = Column(String(100), nullable=False)
+
+class SurveyMBTI(Base):
+  __tablename__ = "SurveyMBTI"
+  id = Column(BigInteger, primary_key=True)
+  user_id = Column(BigInteger, index=True)
+  ei_score = Column(Integer)
+  sn_score = Column(Integer)
+  tf_score = Column(Integer)
+  jp_score = Column(Integer)
+  
+class SurveyLikedFood(Base):
+  __tablename__ = "SurveyLikedFood"
+  id = Column(BigInteger, primary_key=True)
+  user_id = Column(BigInteger, index=True)
+  food_name = Column(String(100))
+  
+class SurveyDislikedFood(Base):
+  __tablename__ = "SurveyDislikedFood"
+  id = Column(BigInteger, primary_key=True)
+  user_id = Column(BigInteger, index=True)
+  food_name = Column(String(100))
+
+class PlaceRecommendationSessions(Base):
+    __tablename__ = "PlaceRecommendationSessions"
+    id = Column(BigInteger, primary_key=True, autoincrement=True)
+    manittee_id = Column(BigInteger, ForeignKey("Users.id"), nullable=False)
+    manitto_id = Column(BigInteger, ForeignKey("Users.id"), nullable=False)
+    week = Column(Integer, nullable=False)
+
+    # 관계 설정 (선택)
+    recommendations = relationship("PlaceRecommendations", back_populates="session")
+
+
+class PlaceRecommendations(Base):
+    __tablename__ = "PlaceRecommendations"
+    id = Column(BigInteger, primary_key=True, autoincrement=True)
+    session_id = Column(BigInteger, ForeignKey("PlaceRecommendationSessions.id"), nullable=False)
+    type = Column(String(20), nullable=False)  # 'cafe' or 'restaurant'
+    name = Column(String(150), nullable=False)
+    category = Column(String(50))
+    opening_hours = Column(String(50))
+
+    session = relationship("PlaceRecommendationSessions", back_populates="recommendations")
